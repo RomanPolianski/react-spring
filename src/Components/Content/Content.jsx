b0df05a34d0c2fbf667a44742e84f9363bdd97c3
Roman <rpol123453@gmail.com>
2022-03-11 12:42:12 +0300

    install classnames

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,30 @@
+const Content = () => {
+  return (
+    <div class="content bg-grey">
+      <div class="container">
+        <section>
+          <input
+            placeholder="Find your project"
+            type="text"
+            class="proj-input"
+          />
+          <div class="project-content">
+            <div class="projects">
+            <div class="project bg-white">
+          <div class="project-icon-header">
+            <img class="project-icon-header"/>
+            <div class="project-header">
+            <h3 class="bold"></h3>
+          </div>
+          </div>
+          <div class="project-description">
+            <p></p>
+          </div>
+        </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
