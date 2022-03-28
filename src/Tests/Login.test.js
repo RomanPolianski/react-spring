2ca56ca33e907c249d0eb694632b11c356723059
Roman <rpol123453@gmail.com>
2022-03-28 10:38:20 +0300

    Add husky

---
+++ src/Tests/Login.test.js
@@ -0,0 +1,10 @@
+import { sendLoginData } from '../Redux/authSlice';
+
+describe('Login success', () => {
+  test('dispatch login action', async () => {
+    const thunk = sendLoginData({ username: 'admin', password: '1234' });
+    const dispatchMock = jest.fn();
+    await thunk(dispatchMock);
+    expect(dispatchMock).toBeCalledTimes(1);
+  });
+});
