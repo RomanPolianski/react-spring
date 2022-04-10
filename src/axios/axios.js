6f6316f7965cf80a76992e0d1906151466b897e7
Roman <rpol123453@gmail.com>
2022-04-10 22:59:29 +0300

    Change axios instance

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 export const API_URL = 'https://server-react-spring.herokuapp.com/api';
 
 const instance = axios.create({
-  withCredentials: true,
   baseURL: API_URL,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
 });
 
 export default instance;
